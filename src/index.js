5f20e4f5899355c591b25f1c93c7304047628725
tyommmm <artyomhovsepyan135@gmail.com>
2022-11-29 18:17:42 +0400

    deployment fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom"
 import './index.css';
 import App from './App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter>
-
         <App />
-
-    </HashRouter>
 
 );
